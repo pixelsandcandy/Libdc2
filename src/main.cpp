1be523ba2f5bb7399d70971662e2648259b861fa
Christopher Miles <smile@christophermil.es>
2016-04-26 17:34:40 -0400

    real init

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,8 @@
 
 //========================================================================
 int main( ){
-	ofSetupOpenGL(1600,600,OF_WINDOW);			// <-------- setup the GL context
+	//ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
+    ofSetupOpenGL(640*3,480,OF_WINDOW);
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,8 +1,6 @@
 #include "ofApp.h"
 
-
-//--------------------------------------------------------------
-void ofApp::setup(){
+void ofApp::setup() {
     ofSetVerticalSync(true);
     ofSetLogLevel(OF_LOG_VERBOSE);
     
@@ -12,51 +10,41 @@ void ofApp::setup(){
     
     // setup() will pick the first camera. Or if there are multiple cameras you
     // can use the number, or the GUID of the camera.
-    camera.setup();
+    //camera.resetBus();
     camera.set1394b(true);
+    camera.setFormat7(true);
+    camera.setBayerMode(DC1394_COLOR_FILTER_RGGB, DC1394_BAYER_METHOD_NEAREST);
+    camera.setup();
     
-    
-    camera.setExposure(0.8);
-    //camera.setBayerMode(DC1394_COLOR_FILTER_BGGR);
-    //camera.setFormat7(true);
-    bayerType = 0;
     /*
      typedef enum {
      DC1394_COLOR_FILTER_RGGB = 512,
      DC1394_COLOR_FILTER_GBRG,
      DC1394_COLOR_FILTER_GRBG,
      DC1394_COLOR_FILTER_BGGR
-     } dc1394color_filter_t;
+     }
      */
     
-    
-    //camera.setImageType( OF_IMAGE_COLOR );
-    //camera.setBayerMode( CV_Bayer
-    
+    //camera.disableBayer();
     // After setup it's still possible to change a lot of parameters. If you want
     // to change a pre-setup parameter, the camera will auto-restart
     
     // Set the window size to the camera size.
-    // ofSetWindowShape(camera.getWidth(), camera.getHeight());
-    
+    //ofSetWindowShape(camera.getWidth(), camera.getHeight());
     rawImage.allocate(camera.getWidth(), camera.getHeight());
     colorImage.allocate(camera.getWidth(), camera.getHeight());
     processed.allocate(camera.getWidth(), camera.getHeight(), OF_IMAGE_COLOR);
-    
 }
 
-//--------------------------------------------------------------
-void ofApp::update(){
+void ofApp::update() {
+    // grabVideo() will place the most recent frame into curFrame. If it's a new
+    // frame, grabFrame returns true. If there are multiple frames available, it
+    // will drop old frames and only give you the newest. This guarantees the
+    // lowest latency. If you prefer to not drop frames, set the second argument
+    // (dropFrames) to false. By default, capture is non-blocking.
+    auto v = camera.grabVideo(curFrame);
     
-    /*if ( true ) {
-        if(camera.grabVideo(curFrame)) {
-            curFrame.update();
-        }
-    }
-    return;*/
-    
-    
-    if(camera.grabVideo(curFrame)) {
+    if(v) {
         curFrame.update();
         rawImage.setFromPixels(curFrame.getPixels().getData(), camera.getWidth(), camera.getHeight());
         
@@ -67,23 +55,27 @@ void ofApp::update(){
         switch(bayerType) {
             case 0:
                 code = CV_BayerBG2RGB;
+                cvCvtColor(rawImage.getCvImage(), colorImage.getCvImage(), CV_BayerBG2RGB); // this seems to best match the colour mode of the camera
                 bayerName = "CV_BayerBG2RGB";
                 break;
             case 1:
                 code = CV_BayerGB2RGB;
+                cvCvtColor(rawImage.getCvImage(), colorImage.getCvImage(), CV_BayerGB2RGB); // this seems to best match the colour mode of the camera
                 bayerName = "CV_BayerGB2RGB";
                 break;
             case 2:
                 code = CV_BayerRG2RGB;
+                cvCvtColor(rawImage.getCvImage(), colorImage.getCvImage(), CV_BayerRG2RGB); // this seems to best match the colour mode of the camera
                 bayerName = "CV_BayerRG2RGB";
                 break;
             case 3:
                 code = CV_BayerGR2RGB;
+                cvCvtColor(rawImage.getCvImage(), colorImage.getCvImage(), CV_BayerGR2RGB); // this seems to best match the colour mode of the camera
                 bayerName = "CV_BayerGR2RGB";
                 break;
         }
         
-        cvCvtColor(rawImage.getCvImage(), colorImage.getCvImage(), CV_BayerRG2BGR); // this seems to best match the colour mode of the camera
+        //cvCvtColor(rawImage.getCvImage(), colorImage.getCvImage(), CV_BayerRG2BGR); // this seems to best match the colour mode of the camera
         
         bayer.setFromPixels(rawImage.getPixels());
         
@@ -105,31 +97,30 @@ void ofApp::update(){
         processed.setFromPixels(colorImage.getPixels());
         
         /*unsigned char * blue = processed.getPixels().getData() + 2;
-        for (int i=0; i<processed.getWidth()*processed.getHeight(); i++) {
-            *blue = pow((float) *blue / 255.0f, 0.9f)  * 255.0f;
-            blue += 3;
-        }*/
+         for (int i=0; i<processed.getWidth()*processed.getHeight(); i++) {
+         *blue = pow((float) *blue / 255.0f, 0.9f)  * 255.0f;
+         blue += 3;
+         }*/
         
         //processed.setFromPixels(rawImage.getPixels(), camera.getWidth(), camera.getHeight() );
         processed.update();
+    } else {
+        
     }
 }
 
-//--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::draw() {
+    // If the camera isn't ready, the curFrame will be empty.
     if(camera.isReady()) {
         // Camera doesn't draw itself, curFrame does.
         curFrame.draw(0, 0);
-        //processed.draw(200,200);
-        //processed.draw(800,0);
-        rawImage.draw(800,0);
-        rgb.draw(800, 300);
+        rawImage.draw(640,0);
+        rgb.draw(1280, 0);
     }
     
     ofDrawBitmapStringHighlight("use the up/down keys: " + bayerName, 10, 20);
 }
 
-//--------------------------------------------------------------
 void ofApp::keyPressed(int key){
     if(key == OF_KEY_UP) {
         bayerType--;
@@ -138,54 +129,4 @@ void ofApp::keyPressed(int key){
         bayerType++;
     }
     bayerType = ofClamp(bayerType, 0, 3);
-}
-
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
 }
